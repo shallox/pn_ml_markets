568929d1ada80c0b490bdd194535fee5dd1d44f9
Jeshua Brennan <jeshuab1@gmail.com>
2023-02-11 20:39:00 +1100

    Adding trained model and base with BTCUSDT back from 10-2017

--- testing.py
+++
@@ -1,7 +0,0 @@
-from datetime import datetime, timedelta
-
-inputa = '2023-10'
-dts_split = inputa.split('-')
-dtn_split = datetime.now().strftime('%Y-%m').split('-')
-new_start_year = (int(dtn_split[0]) - int(dts_split[0])) + 1
-print(new_start_year)
